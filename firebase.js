9729c72b232b11c7384781326acc14c8995e496b
Perebonilado <richad.eradiri@cicod.com>
2022-09-19 17:00:52 +0100

    push notification setup

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getMessaging, getToken } from "firebase/messaging"
 
 
 const firebaseConfig = {
@@ -17,3 +18,29 @@ export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 export const db = getFirestore(app)
 
 export const storage = getStorage(app);
+
+let messaging
+let token
+
+try {
+  messaging = getMessaging(app)
+} catch (error) {
+  console.error(error)
+}
+
+if(messaging){
+  getToken(messaging, { vapidKey: 'BH36-dNfAcFxkW93CZpnSWZ9TYe0-XWdRYuv5auWXdgPgAvLZX4Zp1CAl5JU7E4r22fTmqGXLI59AUIEGNKdCUo' }).then((currentToken) => {
+    if (currentToken) {
+      // Send the token to your server and update the UI if necessary
+      // ...
+    } else {
+      // Show permission request UI
+      console.log('No registration token available. Request permission to generate one.');
+      // ...
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+  });
+}
+
